b5d3d68426bc0b8afc4b901706bd9b9e33d8d6d1
SvenFixe <sven.remond.sti2@gmail.com>
2020-11-25 18:42:02 +0100

    Début de la version mobile

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,15 @@
 function getAge(date) {
-    var today = new Date();
-    var birthDate = new Date(date);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age = age - 1;
-    }
+	var today = new Date();
+	var birthDate = new Date(date);
+	var age = today.getFullYear() - birthDate.getFullYear();
+	var m = today.getMonth() - birthDate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age = age - 1;
+	}
 
-    document.getElementById('age').innerHTML = age + ' ans';
+	document.getElementById("age").innerHTML = age + " ans";
 
-    return age;
+	return age;
 }
 
 getAge("11/27/2001");
-
