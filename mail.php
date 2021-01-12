cc24a9c8a5d72296cba07ae46f291a6b571ed1dc
SvenFixe <sven.remond.sti2@gmail.com>
2021-01-12 17:33:42 +0100

    Correction du mail

--- mail.php
+++ mail.php
@@ -1,3 +1,19 @@
 <?php
+require './mailer/mailFunction.php';
 
-var_dump($_POST);
+$datas = [
+    'HOST' => ['host' => 'mail.sven-remond.fr', 'username' => 'site.cv@sven-remond.fr', 'password' => 'Sven27**'],
+    'FROM' => ['mail' => 'site.cv@sven-remond.fr', 'name' => 'Site CV'],
+    'TO' => ['mail' => 'sven@sven-remond.fr', 'name' => 'Sven']
+];
+
+$object = $_POST['object'];
+
+$message = $_POST['message'] . "\n\n de " . strtoupper($_POST['lastname']) . " " . $_POST['firstname'] . "\nMail : " . $_POST['email'];
+
+$mail = ['Subject' => $object, 'Message' => $message];
+
+sendMail($datas, $mail);
+
+header('Location: ./index.html');
+exit();
